de823626b62f3a1f2488866157e754ab886ead70
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-06-03 22:45:01 -0300

    Renamed database to mongoose

--- src/providers/mongoose/mongoose.module.ts
+++ src/providers/mongoose/mongoose.module.ts
@@ -1,9 +1,9 @@
 
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.provider';
+import { databaseProviders } from './mongoose.provider';
 
 @Module({
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
-export class DatabaseModule {}
+export class MoongoseModule {}
